af284713bf1311b13cde2686e4177f07f2681353
CaroG1986 <carolina.garciao@upb.edu.co>
2026-03-16 19:03:12 -0500

    Cosas nuevas

--- Assets/Main/Scripts/SceneChanger.cs
+++ Assets/Main/Scripts/SceneChanger.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ChangeScene (int IdScene)
+    {
+        SceneManager.LoadScene (IdScene);
     }
 }
