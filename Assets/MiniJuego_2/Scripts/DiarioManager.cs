033299d5b04a5a27c74f0b19077a0d4c145cb54e
Sara Ruiz <sararuizar2006@gmail.com>
2026-05-12 20:06:58 -0500

    0

--- Assets/MiniJuego_2/Scripts/DiarioManager.cs
+++ Assets/MiniJuego_2/Scripts/DiarioManager.cs
@@ -5,32 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class DiarioManager : MonoBehaviour
 {
+    // En el inspector aparecen 3 casillas para asignar los siguientes objetos 
     [Header("Objetos")]
     public GameObject diarioCerrado;
     public GameObject panelCombinacion;
     public GameObject diarioLyra;
 
+    // En el inspector aparecen 3 casillas para asignar los textos que se usaran en la contraseña
     [Header("Textos de las casillas")]
     public TextMeshProUGUI texto1;
     public TextMeshProUGUI texto2;
     public TextMeshProUGUI texto3;
 
+    // En el inspector aparecen 3 casillas para asignar los siguientes colores
     [Header("Colores")]
     public Color colorCorrecto = Color.green;
     public Color colorMalPos = Color.yellow;
     public Color colorIncorrecto = Color.red;
 
+    // En el inspector aparecera el array en ceros que corresponde a los numeros del usuario al iniciar el juego
     public int[] digitos = { 0, 0, 0 };
+    // En el inspector aparecera el array con los numeros de la contraseña correcta
     public int[] codigoCorrecto = { 5, 6, 2 };
 
     void Start()
     {
+        // Cuando le das play se apagan los objetos que asiganste a estas variables
         diarioCerrado.SetActive(false);
         panelCombinacion.SetActive(false);
     }
 
     public void AbrirDiario()
     {
+        // Cuando le das click al objeto que tiene asignado este codigo, prende los objetos que antes tenias apagados
         diarioCerrado.SetActive(true);
         panelCombinacion.SetActive(true);
         ActualizarTextos();
@@ -38,29 +45,36 @@ public class DiarioManager : MonoBehaviour
 
     public void CerrarDiario()
     {
+        // Cuando le das click al objeto que tiene asignado este codigo, apaga los objetos que antes tenias prendidos.
         diarioCerrado.SetActive(false);
         panelCombinacion.SetActive(false);
-        diarioLyra.SetActive(true);
+
     }
 
     public void SubirDigito(int indice)
     {
+        // El objeto que tiene asigando este codigo debe decirme a que numero voy a modificar con el INDICE del array
+        // Una vez haya un indice asignado, al darle click aumenta +1 el valor
         digitos[indice] = (digitos[indice] + 1) % 10;
         ActualizarTextos();
     }
 
     public void BajarDigito(int indice)
     {
+        // El objeto que tiene asigando este codigo debe decirme a que numero voy a modificar con el INDICE del array
+        // Una vez haya un indice asignado, al darle click disminuye -1 el valor >> (-1 para c# es +9)
         digitos[indice] = (digitos[indice] + 9) % 10;
         ActualizarTextos();
     }
 
     void ActualizarTextos()
     {
+        //Usa las nuevas modificaciones hechas en los arrays de los numeros del usuario y las transforma a strings
         texto1.text = digitos[0].ToString();
         texto2.text = digitos[1].ToString();
         texto3.text = digitos[2].ToString();
 
+        //El color base siempre sera blanco
         texto1.color = Color.white;
         texto2.color = Color.white;
         texto3.color = Color.white;
@@ -70,33 +84,41 @@ public class DiarioManager : MonoBehaviour
     {
         Debug.Log("Digitos ingresados: " + digitos[0] + " " + digitos[1] + " " + digitos[2]);
 
+        //Al darle click al boton que ejecuta este codigo, se crea un array NUEVO con los ultimos valores en strings asignados a los textos
         TextMeshProUGUI[] textos = { texto1, texto2, texto3 };
 
+        //Se inicia un for que se repite 3 veces, analizando cada numero del nuevo array para dar feedback visual del codigo del usuario
         for (int i = 0; i < digitos.Length; i++)
         {
             if (digitos[i] == codigoCorrecto[i])
             {
+                //Numero y posicion correctos
                 textos[i].color = colorCorrecto; // Verde
             }
             else if (System.Array.Exists(codigoCorrecto, x => x == digitos[i]))
             {
+                //Numero correcto y mala posicion
                 textos[i].color = colorMalPos; // Amarillo
             }
             else
             {
+                //Ninguna de las anteriores >> todo malo
                 textos[i].color = colorIncorrecto; // Rojo
             }
         }
 
+        //Se inicia un for que COMPRUEBA EN CONJUNTO todo el codigo del usuario con el correcto 
         if (digitos[0] == codigoCorrecto[0] &&
             digitos[1] == codigoCorrecto[1] &&
             digitos[2] == codigoCorrecto[2])
         {
+            //Si es correcto, pasa de escena
             Debug.Log("CORRECTO");
             SceneManager.LoadScene("Corto_3");
         }
         else
         {
+            //Si hay un solo digito malo, no pasa nada, solo marca los colores
             Debug.Log("INCORRECTO");
         }
     }
--- Assets/MiniJuego_2/Scripts/FloatAnim.cs
+++ Assets/MiniJuego_2/Scripts/FloatAnim.cs
@@ -7,19 +7,22 @@ public class FloatAnim : MonoBehaviour
     public float frequency = 1f;     // Velocidad de la oscilación
 
     [Header("Rotación")]
-    public Vector3 rotationSpeed = new Vector3(0, 50f, 0);
+    public Vector3 rotationSpeed = new Vector3(0, 50f, 0); // Un valor de rotacion solo en y
 
     [Header("Opcional")]
     public bool randomOffset = true; // Para que no todos se muevan igual
 
-
+    // Estas variables privadas se escriben aqui afuera para que todo este codigo pueda usarlas
     private Vector3 startPos;
     private float offset;
 
     void Start()
     {
+        //Esto guarda la posicion exacta del objeto al momento de darle play.
         startPos = transform.position;
 
+        //El if pregunta si el bool esta activo o no
+        //En caso de estarlo hace que el offset tenga valores aleatorios
         if (randomOffset)
         {
             offset = Random.Range(0f, Mathf.PI * 2f);
@@ -28,9 +31,12 @@ public class FloatAnim : MonoBehaviour
 
     void Update()
     {
+        //En el update esta constantemente haciendo operaciones con el sin (oscilaciones)
         float y = Mathf.Sin(Time.time * frequency + offset) * amplitude;
+        //Al valor original de la posicion le sumamos el valor de la opreacion anterior solo en y (eje vertical)
         transform.position = startPos + new Vector3(0, y, 0);
-
+       
+        //Rota basandose en lo que tarda en renderizar el ultimo frame multiplicado por el valor de rotacion antes dado
         transform.Rotate(rotationSpeed * Time.deltaTime);
     }
 }
